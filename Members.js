3a91e7bc58c30b25779b9422c6882f052d0c97d9
Jonathan Cain <jcainuk@gmail.com>
2023-07-31 22:17:55 +0100

    Pushing work

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    status: "active",
+    id: 1
+  },
+  {
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    status: "inactive",
+    id: 2
+  },
+  {
+    name: "Robert Johnson",
+    email: "robert.johnson@example.com",
+    status: "active",
+    id: 3
+  }
+];
+
+module.exports = members;
